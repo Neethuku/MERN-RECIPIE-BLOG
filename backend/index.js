f6ae7ec2f386d09d15af219dbbeefb2c7d40b6ef
Neethuku <neethuku02@gmail.com>
2024-03-25 10:46:04 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use('/api/comment',commentRoutes);
 
 app.use(express.static(path.join(__dirname,'/frontend/dist')));
 
-app.get('*',(req,res) => {
+app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
